fd594e746e24df91ff9cb24571620cbe923b778c
Aayush Joshi <48806014+iamcodenivorous@users.noreply.github.com>
2023-05-09 23:30:16 +0530

    added initial file structure and home page

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const ejs = require("ejs")
+app = express()
+require('dotenv').config()
+const PORT = process.env.PORT
+const DATABASE_URL = process.env.DATABASE_URL
+app.use(express.static(__dirname + '/public'))
+app.set("view engine", 'ejs')
+
+app.get("/", (req, res)=>{
+    res.render("index")
+})
+app.get("/contactus", (req, res)=>{
+    res.render("contactus")
+})
+app.get("/donate", (req, res)=>{
+    res.render("donate")
+})
+app.listen(PORT, (err)=>{
+    if(err)
+        console.log("error starting server")
+    else   
+        console.log(`server started successfully in port ${PORT}`)
+})
